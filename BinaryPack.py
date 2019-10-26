77c20b96ffdccc788e362d1ec55c371e0fb7f36d
Aaron Sun <aaronsun2000@berkeley.edu>
2019-10-26 12:37:09 -0700

    goodbye binarypack, hello rectangular

--- BinaryPack.py
+++ BinaryPack.py
@@ -21,27 +21,7 @@ class BinaryPack(Pack):
     def pack(self):
         return None
 
-    def optimize_dino(self, dino):
 
-        def rotate_dino(angle):
-
-            rotated = [[0 for x in range(len(dino))] for y in range(len(dino[0]))]
-
-            origin_x = len(dino[0]) // 2
-            origin_y = len(dino) // 2
-            rotMat = [[cos(angle), -1 * sin(angle)],
-                      [sin(angle), cos(angle)]]
-
-            for y in range(len(rotated)):
-                for x in range(len(rotated[0])):
-                    if dino[y][x]:
-                        vector = [x - origin_x, origin_y - y]
-                        rot_vector = np.matmul(rotMat, vector)
-                        rot_vector[0] = max(-origin_x, min(rot_vector[0], origin_x))
-                        rot_vector[1] = max(-origin_y, min(rot_vector[1], origin_y))
-                        rotated[int(round(origin_y - rot_vector[1]))][int(round(origin_x + rot_vector[0]))] = 1
-
-            return rotated
 
 
 a = BinaryPack()
--- RectangularPack.py
+++ RectangularPack.py
@@ -3,6 +3,11 @@ test_matrix2 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 test_matrix3 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 test_matrix4 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
+from math import sin, cos, pi
+import numpy as np
+
+from Pack import Pack
+
 
 class RectangularPack():
 
@@ -12,16 +17,28 @@ class RectangularPack():
 
     def reduce_matrix(dino_matrix, rotation_angle = 0):
         """takes in a matrix, and returns the minimum sized rectangle. """
-        def rotate_matrix(matrix, angle):
-            """use aaron's matrix rotator"""
-            """helper function that returns rotated matrix"""
-            if angle == 0:
-                rotated_matrix = matrix
-            else:
-                """need to implement rotation with linear alg"""
-                #rotation matrix
-                #rotated_matrix = rotation_matrix @ matrix
-            return rotated_matrix
+
+        def optimize_dino(self, dino):
+
+            def rotate_dino(angle):
+
+                rotated = [[0 for x in range(len(dino))] for y in range(len(dino[0]))]
+
+                origin_x = len(dino[0]) // 2
+                origin_y = len(dino) // 2
+                rotMat = [[cos(angle), -1 * sin(angle)],
+                          [sin(angle), cos(angle)]]
+
+                for y in range(len(rotated)):
+                    for x in range(len(rotated[0])):
+                        if dino[y][x]:
+                            vector = [x - origin_x, origin_y - y]
+                            rot_vector = np.matmul(rotMat, vector)
+                            rot_vector[0] = max(-origin_x, min(rot_vector[0], origin_x))
+                            rot_vector[1] = max(-origin_y, min(rot_vector[1], origin_y))
+                            rotated[int(round(origin_y - rot_vector[1]))][int(round(origin_x + rot_vector[0]))] = 1
+
+                return rotated
 
         def remove_edges(matrix):
             """Inputting a matrix of ones and zeros, """
