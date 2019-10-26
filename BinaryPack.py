e84f7564922dbfc63bbb03225f8c1e3a60131b22
Aaron Sun <aaronsun2000@berkeley.edu>
2019-10-26 16:39:15 -0700

    unnesting the rotation

--- BinaryPack.py
+++ BinaryPack.py
@@ -43,69 +43,49 @@ class BinaryPack(Pack):
 
     def reduce_matrix(self, dino_matrix):
         """takes in a matrix, and returns the minimum sized rectangle. through rotating and cutting"""
-
-        def rotate_dino(angle):
-
-            length = int(sqrt((len(dino_matrix) ** 2 + len(dino_matrix[0]) ** 2)))
-            rotated = [[0 for x in range(length)] for y in range(length)]
-
-            origin_x = len(dino_matrix[0]) // 2
-            origin_y = len(dino_matrix) // 2
-            rotMat = [[cos(angle), -1 * sin(angle)],
-                      [sin(angle), cos(angle)]]
-
-            for y in range(len(dino_matrix)):
-                for x in range(len(dino_matrix[0])):
-                    if dino_matrix[y][x]:
-                        vector = [x - origin_x, origin_y - y]
-                        rot_vector = np.matmul(rotMat, vector)
-                        rotated[int(round(length//2 - rot_vector[1]))][int(round(length//2 + rot_vector[0]))] = 1
-
-            return rotated
-
-        def remove_edges(matrix):
-            """Inputting a matrix of ones and zeros, """
-            rows = len(matrix)
-            """remove excess rows"""
-            new_matrix = []
-            for i in range(rows):
-                if not sum(matrix[i]) == 0:
-                    """delete row"""
-                    new_matrix.append(matrix[i])
-            cols = len(new_matrix[0])
-            """remove excess columns"""
-            col_to_keep = []
-            for i in range(cols):
-                columns = [row[i] for row in new_matrix]
-                if not sum(columns) == 0:
-                    col_to_keep += [i]
-
-            new_matrix = [[rows[i] for i in col_to_keep] for rows in new_matrix]
-
-            return new_matrix
-
-        possible_matrixes = [remove_edges(rotate_dino(angle)) for angle in range(0, 181, 5)]
+        possible_matrixes = [self.remove_edges(self.rotate_dino(dino_matrix, angle)) for angle in range(0, 181, 5)]
         area_matrixes = [len(matrix) * len(matrix[0]) for matrix in possible_matrixes]
         small_index = area_matrixes.index(min(area_matrixes))
         return possible_matrixes[small_index]
 
+    def rotate_dino(self, dino_matrix, angle):
 
-def rotate_dino(dino_matrix, angle):
-    length = int(sqrt(len(dino_matrix) ** 2 + len(dino_matrix[0]) ** 2))
-    rotated = [[0 for x in range(length)] for y in range(length)]
+        length = int(sqrt((len(dino_matrix) ** 2 + len(dino_matrix[0]) ** 2)))
+        rotated = [[0 for x in range(length)] for y in range(length)]
 
-    origin_x = len(dino_matrix[0]) // 2
-    origin_y = len(dino_matrix) // 2
-    rotMat = [[cos(angle), -1 * sin(angle)],
-              [sin(angle), cos(angle)]]
+        origin_x = len(dino_matrix[0]) // 2
+        origin_y = len(dino_matrix) // 2
+        rotMat = [[cos(angle), -1 * sin(angle)],
+                  [sin(angle), cos(angle)]]
 
-    for y in range(len(dino_matrix)):
-        for x in range(len(dino_matrix[0])):
-            if dino_matrix[y][x]:
-                vector = [x - origin_x, origin_y - y]
-                rot_vector = np.matmul(rotMat, vector)
-                rotated[int(round(length//2 - rot_vector[1]))][int(round(length//2 + rot_vector[0]))] = 1
+        for y in range(len(dino_matrix)):
+            for x in range(len(dino_matrix[0])):
+                if dino_matrix[y][x]:
+                    vector = [x - origin_x, origin_y - y]
+                    rot_vector = np.matmul(rotMat, vector)
+                    rotated[int(round(length//2 - rot_vector[1]))][int(round(length//2 + rot_vector[0]))] = 1
 
-    return rotated
+        return rotated
+
+    def remove_edges(self, matrix):
+        """Inputting a matrix of ones and zeros, """
+        rows = len(matrix)
+        """remove excess rows"""
+        new_matrix = []
+        for i in range(rows):
+            if not sum(matrix[i]) == 0:
+                """delete row"""
+                new_matrix.append(matrix[i])
+        cols = len(new_matrix[0])
+        """remove excess columns"""
+        col_to_keep = []
+        for i in range(cols):
+            columns = [row[i] for row in new_matrix]
+            if not sum(columns) == 0:
+                col_to_keep += [i]
+
+        new_matrix = [[rows[i] for i in col_to_keep] for rows in new_matrix]
+
+        return new_matrix
 
 
