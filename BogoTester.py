005a1c8d90bb8e45cb2bd6268f416097614971c1
Tristan Yoo <tristan.yoo@berkeley.edu>
2019-10-26 17:30:42 -0700

    wowow

--- BogoTester.py
+++ BogoTester.py
@@ -2,7 +2,8 @@ from ImageReader import ImageReader
 from BogoPack import BogoPack
 from Display import Display
 
-test = [ImageReader.text_to_array("triceratops.dino"), ImageReader.text_to_array("pterodactyl.dino")]
+test = [ImageReader.text_to_array("triceratops.dino"), ImageReader.text_to_array("pterodactyl.dino"),
+        ImageReader.text_to_array("trex.dino")]
 a = BogoPack(400, 300, test)
 image = Display.tray_to_image(a.pack())
 image.show()
